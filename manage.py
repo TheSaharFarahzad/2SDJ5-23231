33670025a89f1ed28f9056e7e911bad7b94f5e75
Sahar <shr.farahzad@gmail.com>
2025-02-05 17:28:43 +0330

    Dynamically set DJANGO_SETTINGS_MODULE based on ENVIRONMENT variable

--- manage.py
+++ manage.py
@@ -6,7 +6,11 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurant_booking.settings')
+    environment = os.getenv("ENVIRONMENT", "local")
+    os.environ.setdefault(
+        "DJANGO_SETTINGS_MODULE", f"restaurant_booking.settings.{environment}"
+    )
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -18,5 +22,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
