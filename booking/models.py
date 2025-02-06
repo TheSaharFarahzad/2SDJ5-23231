d0cfab52f38b269061aa6e715f9230379e67487b
Sahar <shr.farahzad@gmail.com>
2025-02-07 01:10:43 +0330

    Refactor: Moved business logic from views to models

--- booking/models.py
+++ booking/models.py
@@ -6,6 +6,16 @@ from django.conf import settings
 User = get_user_model()
 
 
+class TableManager(models.Manager):
+    def get_best_available_table(self, number_of_seats):
+        available_tables = self.filter(total_seats__gte=number_of_seats).order_by(
+            "price", "total_seats"
+        )
+        if not available_tables.exists():
+            raise ValidationError("No available table for this number of seats.")
+        return available_tables.first()
+
+
 class Table(models.Model):
     table_number = models.PositiveSmallIntegerField(unique=True)
     total_seats = models.PositiveSmallIntegerField()
@@ -13,9 +23,20 @@ class Table(models.Model):
         max_digits=10, decimal_places=2, default=settings.DEFAULT_TABLE_PRICE
     )
 
+    objects = TableManager()
+
     def clean(self):
         if self.total_seats < 4 or self.total_seats > 10:
             raise ValidationError("Total seats must be between 4 and 10.")
+
+    def calculate_cost(self, number_of_seats):
+        seat_price = self.price / self.total_seats
+        total_seat_cost = number_of_seats * seat_price
+
+        if number_of_seats == self.total_seats:
+            return (self.total_seats - 1) * seat_price
+
+        return total_seat_cost
 
     def __str__(self):
         return f"Table {self.table_number} ({self.total_seats} seats)"
@@ -32,6 +53,14 @@ class Reservation(models.Model):
     cost = models.DecimalField(max_digits=10, decimal_places=2)
     booked_at = models.DateTimeField(auto_now_add=True)
     active = models.BooleanField(default=True)
+
+    @classmethod
+    def validate_number_of_seats(cls, number_of_seats):
+        if number_of_seats < 4:
+            raise ValidationError("Number of seats must be at least 4.")
+        if number_of_seats % 2 != 0:
+            number_of_seats += 1
+        return number_of_seats
 
     def __str__(self):
         return f"Reservation by {self.user.username} for {self.number_of_seats} seats at Table {self.table.table_number}"
--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,9 @@
 from rest_framework import permissions, viewsets, status
-from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import ValidationError as DRFValidationError
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from django.core.exceptions import ValidationError as DjangoValidationError
+
 from .serializers import (
     ReservationListSerializer,
     ReservationCreateSerializer,
@@ -17,8 +19,11 @@ class TableViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         if Table.objects.count() >= 10:
-            raise ValidationError("Cannot create more than 10 tables.")
-        serializer.save()
+            raise DRFValidationError("Cannot create more than 10 tables.")
+        try:
+            serializer.save()
+        except DjangoValidationError as e:
+            raise DRFValidationError(e.messages)
 
 
 class ReservationViewSet(viewsets.ModelViewSet):
@@ -33,11 +38,16 @@ class ReservationViewSet(viewsets.ModelViewSet):
             return ReservationListSerializer
         return ReservationCreateSerializer
 
-    @action(detail=True, methods=["post"])
+    def validate_cancel_request(self, reservation, user):
+        if reservation.user != user:
+            raise DRFValidationError("You can only cancel your own reservations.")
+        if not reservation.active:
+            raise DRFValidationError("This reservation is already cancelled.")
+
+    @action(detail=True, methods=["post"], name="cancel")
     def cancel(self, request, pk=None):
         reservation = self.get_object()
-        if reservation.user != request.user:
-            raise ValidationError("You can only cancel your own reservations.")
+        self.validate_cancel_request(reservation, request.user)
 
         reservation.active = False
         reservation.save()
@@ -46,49 +56,15 @@ class ReservationViewSet(viewsets.ModelViewSet):
             status=status.HTTP_200_OK,
         )
 
-    def get_valid_number_of_seats(self):
-        number_of_seats = self.request.data.get("number_of_seats")
-
-        if number_of_seats is None:
-            raise ValidationError("Number of seats is required.")
-
-        try:
-            number_of_seats = int(number_of_seats)
-        except ValueError:
-            raise ValidationError("Invalid number of seats.")
-
-        if number_of_seats < 4:
-            raise ValidationError("Number of seats must be at least 4.")
-
-        if number_of_seats % 2 != 0:
-            number_of_seats += 1
-
-        return number_of_seats
-
-    def get_best_available_table(self, number_of_seats):
-        available_tables = Table.objects.filter(
-            total_seats__gte=number_of_seats
-        ).order_by("price", "total_seats")
-
-        if not available_tables.exists():
-            raise ValidationError("No available table for this number of seats.")
-
-        return available_tables.first()
-
-    def calculate_cost(self, table, number_of_seats):
-        seat_price = table.price / table.total_seats
-        total_seat_cost = number_of_seats * seat_price
-
-        if number_of_seats == table.total_seats:
-            return (table.total_seats - 1) * seat_price
-
-        return total_seat_cost
-
     def perform_create(self, serializer):
-        number_of_seats = self.get_valid_number_of_seats()
-        best_table = self.get_best_available_table(number_of_seats)
-
-        cost = self.calculate_cost(best_table, number_of_seats)
+        try:
+            number_of_seats = Reservation.validate_number_of_seats(
+                self.request.data.get("number_of_seats")
+            )
+            best_table = Table.objects.get_best_available_table(number_of_seats)
+            cost = best_table.calculate_cost(number_of_seats)
+        except DjangoValidationError as e:
+            raise DRFValidationError(e.messages)
 
         reservation = serializer.save(
             user=self.request.user,
@@ -96,6 +72,5 @@ class ReservationViewSet(viewsets.ModelViewSet):
             cost=cost,
             active=True,
         )
-
         response_serializer = ReservationListSerializer(reservation)
         return Response(response_serializer.data, status=status.HTTP_201_CREATED)
